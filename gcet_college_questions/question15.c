944dd73b0627616108f7418072e095c77038aa35
vineetsinghabcd321 <vineetsingh953250@gmail.com>
2023-12-16 18:34:13 +0530

    first commit

--- gcet_college_questions/question15.c
+++ gcet_college_questions/question15.c
@@ -1,5 +1,19 @@
 //WAP to print the fibonacci series.
 #include <stdio.h>
+
 int main() {
-    
+    int n, first = 0, second = 1, next;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci series:\n");
+
+    for (int i = 0; i <= n; i++) {
+        printf("%d\n", first);
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return 0;
 }
