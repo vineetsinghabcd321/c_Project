8508ccffa7e28b7eb0b0bbbca976d0e7def8d47c
vineetsinghabcd321 <vineetsingh953250@gmail.com>
2023-12-17 23:32:13 +0530

    first commit

---
+++ basic_questions/question4.c
@@ -0,0 +1,10 @@
+// write a program , find the area of circle whose readius if 6cm.
+#include <stdio.h>
+int main() {
+    int raidus = 6;
+    float area;
+    // area of cicrle
+    area = 3.14*6*6;
+    printf("area of circle is = %f", area);
+    return 0;
+}
