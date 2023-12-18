4333e76d7c65ca21f80f4962e464afed44438804
vineetsinghabcd321 <vineetsingh953250@gmail.com>
2023-12-18 23:22:32 +0530

    first commit

--- gcet_college_questions/question18.c
+++ gcet_college_questions/question18.c
@@ -1,1 +1,16 @@
 // write a  program to find reverse of a program.
+#include <stdio.h>
+int main() {
+    int num1, num2, remainder;
+    printf("Enter the number");
+    scanf("%d", &num1);
+
+    while(num1>0) {
+        remainder = num1 %10;
+        num2 = num2*10 + remainder;
+        num1 /= 10;
+    } 
+    printf("reverse of a number is %d \n", num2);
+    return 0;
+}
+
